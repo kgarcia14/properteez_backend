b840ee026cce4ded0a475e58831aa8b3c610e52b
Kurtis Garcia <kurtisgarcia14@gmail.com>
2023-08-19 12:37:19 -0400

    added file upload functionality which saves the uploaded image to a local file directory named images

--- db/schema.sql
+++ db/schema.sql
@@ -27,10 +27,11 @@ CREATE TABLE properties (
     renter_email VARCHAR (200) NOT NULL,
     lease_term VARCHAR (200) NOT NULL,
     rent_amount INT NOT NULL,
-    rent_status VARCHAR (200) NOT NULL
+    rent_status VARCHAR (200) NOT NULL,
+    property_image VARCHAR (200) NOT NULL
 );
 
 -- Insert data into a table
-INSERT INTO properties(user_id, street, city, state, zip, mortgage_amount, vacancy, renter_name, renter_number, renter_email, lease_term, rent_amount, rent_status)
-VALUES (1, '14 Forrest Hills Dr', 'Atlanta', 'Ga', '30362', 2200, 'Occupied', 'John & Laura', '465-887-2304', 'John@gmail.com', '6/20/23 - 6/20/25', 3000, 'Past Due');
+INSERT INTO properties(user_id, street, city, state, zip, mortgage_amount, vacancy, renter_name, renter_number, renter_email, lease_term, rent_amount, rent_status, property_image)
+VALUES (1, '14 Forrest Hills Dr', 'Atlanta', 'Ga', '30362', 2200, 'Occupied', 'John & Laura', '465-887-2304', 'John@gmail.com', '6/20/23 - 6/20/25', 3000, 'Past Due', 'image path');
 
