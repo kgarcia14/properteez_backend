0f72161a05033dcbb3a6d9ab4ad37db0f4033c28
Kurtis Garcia <kurtisgarcia@Kurtiss-MacBook-Pro.local>
2023-12-19 13:28:10 -0500

    testing

--- routes/users.js
+++ routes/users.js
@@ -50,6 +50,7 @@ router.post('/register', async (req, res) => {
             secure: true,
             path: '/',
             sameSite: 'none',
+            maxAge: 60000,
         })
 
         res.status(201).json({results: results[0], accessToken: accessToken});
