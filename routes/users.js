053661cb70457abcf8b6169ab2a272d989f7f910
Kurtis Garcia <kurtisgarcia14@gmail.com>
2023-12-21 16:06:43 -0500

    test

--- routes/users.js
+++ routes/users.js
@@ -41,7 +41,7 @@ router.post('/register', async (req, res) => {
         const refreshToken = generateRefreshToken({user: results[0].user_email});
 
         res.cookie('jwt', refreshToken, {
-            domain: 'properteez.kurtisgarcia.dev',
+            domain: 'properteez.kgbackend.com',
             maxAge: 60000,
             httpOnly: true,
             secure: true,
