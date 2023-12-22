b44465128ae44090d1725c529d955ba79a697faa
Kurtis Garcia <kurtisgarcia14@gmail.com>
2023-12-22 14:34:09 -0500

    FINALLY got cookies to work in development and deployment

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const cors = require('cors');
 
 if (process.env.NODE_ENV === 'development') {
     app.use(cors({
-        origin: 'http://localhost:3000/',
+        origin: 'http://localhost:3000',
         credentials: true,
     }));
 } else {
