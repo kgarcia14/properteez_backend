7f3a315cad994629f22a730a3cb7a2df11458c28
Kurtis Garcia <kurtisgarcia14@gmail.com>
2024-01-14 14:34:02 -0500

    added lease start and lease date in addition to some styling

--- db/schema.sql
+++ db/schema.sql
@@ -20,18 +20,19 @@ CREATE TABLE properties (
     city VARCHAR (200) NOT NULL,
     state VARCHAR (200) NOT NULL,
     zip VARCHAR (200) NOT NULL,
+    home_type VARCHAR (200) NOT NULL,
     mortgage_amount VARCHAR (200) NOT NULL,
     vacancy VARCHAR (200) NOT NULL,
     renter_name VARCHAR (200) NOT NULL,
     renter_number VARCHAR (200) NOT NULL,
     renter_email VARCHAR (200) NOT NULL,
-    lease_term VARCHAR (200) NOT NULL,
+    lease_start VARCHAR (200) NOT NULL,
+    lease_end VARCHAR (200) NOT NULL,
     rent_amount VARCHAR (200) NOT NULL,
     rent_status VARCHAR (200) NOT NULL,
     property_image VARCHAR (200) NOT NULL
 );
 
 -- Insert data into a table
-INSERT INTO properties(user_id, street, city, state, zip, mortgage_amount, vacancy, renter_name, renter_number, renter_email, lease_term, rent_amount, rent_status, property_image)
-VALUES (1, '14 Forrest Hills Dr', 'Atlanta', 'Ga', '30362', 2200, 'Occupied', 'John & Laura', '465-887-2304', 'John@gmail.com', '6/20/23 - 6/20/25', 3000, 'Past Due', 'image path');
+INSERT INTO properties(user_id, street, city, state, zip, home_type, mortgage_amount, vacancy, renter_name, renter_number, renter_email, lease_start, lease_end, rent_amount, rent_status, property_image) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16) RETURNING *', [data.user_id, data.street, data.city, data.state, data.zip, data.home_type, data.mortgage_amount, data.vacancy, data.renter_name, data.renter_number, data.renter_email, data.lease_start, data.lease_end, data.rent_amount, data.rent_status, imageUrl]);
 
